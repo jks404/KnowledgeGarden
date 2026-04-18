b5d6c7e826126386d1ace684cebd99b5c00e6e5e
jks <jasjitk2410@gmail.com>
2026-04-18 17:30:29 -0500

    Quartz sync: Apr 18, 2026, 5:30 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -12,9 +12,7 @@ const config: QuartzConfig = {
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
-    analytics: {
-      provider: "plausible",
-    },
+    analytics: null,
     locale: "en-US",
     baseUrl: "https://garden.jks404.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
